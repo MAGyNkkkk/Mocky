9890f6d1f966e4a0779df2a37a43a5428a7ae511
MAGyNkkkk <143349749+MAGyNkkkk@users.noreply.github.com>
2023-12-27 17:45:00 -0300

    Mocky Language

--- InLib.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-python -m pip install --upgrade pip
-pip install pyinstaller
-pip install pyqt5
--- MockySetup.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-start InPy.bat
-timeout /t 150 /nobreak >nul
-start InLib.bat
-timeout /t 15 /nobreak >nul
-start Mocky.exe
-pause
